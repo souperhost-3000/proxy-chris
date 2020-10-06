1a12836e1ed8480bf7998c0ce3de44bad93efd91
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-10-06 16:01:00 -0700

    Updated proxy

--- proxy/public/server.js
+++ proxy/public/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = process.env.PORT || 3010;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname)))
+
+app.use(express.static(path.join(__dirname)));
+// app.use('/availability/:listing_id', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+// app.use('/api/listings', createProxyMiddleware({ target: 'http://localhost:3007', changeOrigin: true }));
+// app.use('/api/images/:listing_id', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
+app.use('/api/reviews/:listing_id', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+
 
 app.listen(PORT, (err) => {
   if (err) {
