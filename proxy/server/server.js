2fc5e7bc2dd54018f9a9c87d953c21c6a4fbbfc0
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-10-07 11:34:39 -0700

    Updated

--- proxy/server/server.js
+++ proxy/server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3010;
 
 app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 // app.use('/availability/:listing_id', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 // app.use('/api/listings', createProxyMiddleware({ target: 'http://localhost:3007', changeOrigin: true }));
 // app.use('/api/images/:listing_id', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
